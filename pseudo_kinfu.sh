5b3c9a5d61b4e9af4bfb0e3730d5501ee42d3840
ttoocs <ttoocs@gmail.com>
2017-08-05 11:22:18 -0600

    Fixed logical bug where integrate would be using the first set of 50 images for pseudo_kinfu.sh

--- pseudo_kinfu.sh
+++ pseudo_kinfu.sh
@@ -1,8 +1,23 @@
 rm 100-0.log
-for i in {0..500..50}; do
-local2Global cv.traj fun$i.traj $i $((i+50));
+NUMPCD=`cat cv.traj  | tail -n 5 | head -n 1 | cut -d' ' -f 3`
+for i in `seq 0 1 $NUMPCD`; do
+S=$((i*50))
+E=$((S+50))
+echo Start: $S
+echo End: $E
+local2Global cv.traj fun$i.traj $S $E;
 cat fun$i.traj >> 100-0.log;
-Integrate --ref_traj fun$i.traj --resolution 12 --oni_file ../ --save_to fun$i.pcd --camera ../cam.param & 
-done
+rm -rf int$i;
+mkdir int$i;
+  cd int$i;
+  mkdir colourframes
+  mkdir depthframes
+  for j in `seq $S 1 $E`; do
+    ln -s ../../../colourframes/Image$j.jpg ./colourframes/Image$((j-S)).jpg
+    ln -s ../../../depthframes/Image$j.png ./depthframes/Image$((j-S)).png
+  done;
+  cd ..
+Integrate --ref_traj fun$i.traj --resolution 12 --oni_file ./int$i/ --save_to cloud_bin_$i.pcd --camera ../cam.param ;
+done 
 wait
 
