b244e4a6ccda7996201dc5381b8ed0b90588b679
ttoocs <ttoocs@gmail.com>
2017-08-08 17:18:15 -0600

    Only compute what exists for genTraj

--- genTraj.sh
+++ genTraj.sh
@@ -1,6 +1,9 @@
+#!/bin/bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )" 
-for i in {0..500}; do
-echo $i $i $((i+1)) 
-$DIR/build/bin/akaze_match ../colourframes/Image$i.jpg ../colourframes/Image$((i+1)).jpg | grep -A4 Mat:\ H  | tail -n 4
+
+NUMFRAMES=`ls ../colourframes/Image*.* | wc -l`
+for i in `seq 0 1 $(NUMFRAMES-2)`; do
+  echo $i $i $((i+1)) 
+  $DIR/build/bin/akaze_match ../colourframes/Image$i.jpg ../colourframes/Image$((i+1)).jpg | grep -A4 Mat:\ H  | tail -n 4
 done | tee cv.traj 
