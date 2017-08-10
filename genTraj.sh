e9bb328341fc23bb972db8bc18e9ef77f137f9e5
ttoocs <ttoocs@gmail.com>
2017-08-10 13:25:51 -0600

    Fixed silly error.

--- genTraj.sh
+++ genTraj.sh
@@ -3,7 +3,7 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )" 
 
 NUMFRAMES=`ls ../colourframes/Image*.* | wc -l`
-for i in `seq 0 1 $(NUMFRAMES-2)`; do
+for i in `seq 0 1 $((NUMFRAMES-2))`; do
   echo $i $i $((i+1)) 
   $DIR/build/bin/akaze_match ../colourframes/Image$i.jpg ../colourframes/Image$((i+1)).jpg | grep -A4 Mat:\ H  | tail -n 4
 done | tee cv.traj 
