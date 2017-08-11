d403b12ac34401d100735786f2604fece37afd18
ttoocs <ttoocs@gmail.com>
2017-08-11 15:15:36 -0600

    initalizations

--- opencv_traj.cpp
+++ opencv_traj.cpp
@@ -3,6 +3,24 @@
 #include <boost/filesystem.hpp>
 
 #include "er_helper.h"
-int main(int argc, char * argv[]){
 
+
+std::string dir = "../";
+
+void parseArgs(int argc,char *argv[]){
+  for(int i = 1; i < argc; i++){
+    std::string arg = std::string(argv[i]);
+    std::transform(arg.begin(), arg.end(), arg.begin(), ::tolower);
+
+    if (arg == "-dir" ){
+      dir = std::string(argv[++i]);
+    }else{
+      std::cout << " Unknown arg: " << argv[i] << std::endl;
+    }
+  }
+}    
+
+
+int main(int argc, char * argv[]){
+  parseArgs(argc,argv); 
 }
